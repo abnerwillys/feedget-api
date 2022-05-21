b1696d645490712c27d343348f543d14187288b7
Abner Willys <abwillys001@gmail.com>
2022-05-21 20:06:53 -0300

    feat: create route to register a new feedback

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import express from 'express'
+import { prima } from './prisma'
 
 const app = express()
 
-app.get('/users', (req, res) => {
-  return res.send('Hello World!')
+app.use(express.json())
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body
+
+  const feedback = await prima.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+
+  return res.status(201).json({ data: feedback })
 })
 
 app.listen(3333, () => console.log('HTTP server is Running...🚀'))
